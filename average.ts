1a804332e046d8c24a0f8528039297310c85c231
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-05-10 12:02:42 +0200

    Create average.ts

---
+++ average.ts
@@ -0,0 +1,8 @@
+/**
+ * @function
+ * @param list List of number elements
+ * @returns number, the average amount from the number array
+ */
+export function average(list : number[]) : number {
+    return list.reduce((last, current) => last + current, 0) / list.length || 0;
+}
